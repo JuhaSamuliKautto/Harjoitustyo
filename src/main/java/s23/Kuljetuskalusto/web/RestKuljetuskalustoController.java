c69d657cdd79d09e48446e812591f5ac3ac8b23c
JuhaSamuliKautto <juha.kautto@myy.haaga-helia.fi>
2023-10-15 00:33:41 +0300

    RESTia parannettu

--- src/main/java/s23/Kuljetuskalusto/web/RestKuljetuskalustoController.java
+++ src/main/java/s23/Kuljetuskalusto/web/RestKuljetuskalustoController.java
@@ -4,43 +4,67 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import s23.Kuljetuskalusto.domain.Ajoneuvo;
 import s23.Kuljetuskalusto.domain.AjoneuvoRepository;
+//import s23.Kuljetuskalusto.domain.TyyppiRepository;
 
-
+@RestController
 public class RestKuljetuskalustoController {
 	
-	// @Autowired-annotaatio: Tällä annotaatiolla injektoidaan BookstoreRepository-riippuvuus, jota käytetään kirjojen tietokantatoimintoihin.
+
+	
+	// @Autowired-annotaatio: Tällä annotaatiolla injektoidaan AjoneuvoRepository-riippuvuus, jota käytetään ajoneuvojen tietokantatoimintoihin.
 	@Autowired
 		private AjoneuvoRepository arepository;
-			
-			// @GetMapping("/ajoneuvot"): Tämä annotaatio määrittää toiminnon, kun käyttäjä tekee GET-pyynnön osoitteeseen "/ajoneuvot".
+	
+	/*@Autowired 
+		private TyyppiRepository trepository;*/
+	
+	
+	// @GetMapping("/ajoneuvot"): Tämä annotaatio määrittää toiminnon, kun käyttäjä tekee GET-pyynnön osoitteeseen "/ajoneuvot".
 			//Metodi hakee kaikki ajoneuvot tietokannasta arepository.findAll() -kutsulla ja palauttaa ne JSON-muodossa.
 			// Tämä mahdollistaa kaikkien ajoneuvojen hakemisen REST-rajapinnan kautta.
 			@GetMapping("/ajoneuvot")
-			public List<Ajoneuvo> ajoneuvoListRest() {	
+			public List<Ajoneuvo> getAjoneuvot() {
 		        return (List<Ajoneuvo>) arepository.findAll();
 			}
 			
-			// @GetMapping("/ajoneuvo/{id}"): Tämä annotaatio määrittää toiminnon, kun käyttäjä tekee GET-pyynnön osoitteeseen "/ajoneuvo/{id}", 
+			
+			// @GetMapping("/ajoneuvot/{id}"): Tämä annotaatio määrittää toiminnon, kun käyttäjä tekee GET-pyynnön osoitteeseen "/ajoneuvo/{id}", 
 			// missä {id} korvataan ajoneuvon tunnisteella. Metodi hakee ajoneuvon tietokannasta annetun ajoneuvoId-parametrin perusteella arepository.findById(ajoneuvoId) -kutsulla 
 			// ja palauttaa ajoneuvon tiedot JSON-muodossa. Tämä mahdollistaa yksittäisen ajoneuvon hakemisen REST-rajapinnan kautta.
-			@GetMapping("/ajoneuvo/{id}")
+			@GetMapping("/ajoneuvot/{id}")
 		    public Optional<Ajoneuvo> findAjoneuvoRest(@PathVariable("id") Long ajoneuvoId) {	
 		    	return arepository.findById(ajoneuvoId);
 			} 
 			
-			// @PostMapping("/ajoneuvo"): Tämä annotaatio määrittää toiminnon, kun käyttäjä lähettää POST-pyynnön osoitteeseen "/ajoneuvo" ja antaa 
+			// @PostMapping("/ajoneuvot"): Tämä annotaatio määrittää toiminnon, kun käyttäjä lähettää POST-pyynnön osoitteeseen "/ajoneuvot" ja antaa 
 			// JSON-muotoisen kirjatiedon @RequestBody-annotaation avulla. Metodi tallentaa ajoneuvon tietokantaan käyttäen arepository.save(ajoneuvo) -kutsua 
 			// ja palauttaa tallennetun ajoneuvon tiedot JSON-muodossa. Tämä mahdollistaa uuden ajoneuvon lisäämisen REST-rajapinnan kautta.
-			@PostMapping("/ajoneuvo")
-		    public Ajoneuvo save(@RequestBody Ajoneuvo ajoneuvo){
-		        return arepository.save(ajoneuvo);
-		        
-			}	
-	}
+			@PostMapping("/ajoneuvot")
+			public Ajoneuvo postAjoneuvo(@RequestBody Ajoneuvo ajoneuvo) {
+			    return arepository.save(ajoneuvo);
+			}
+			
+			// muokkaa autoa
+			@PutMapping("/ajoneuvot/{id}")
+			public Ajoneuvo putAjoneuvo(@PathVariable Long id, @RequestBody Ajoneuvo editedAjoneuvo) {
+				return arepository.save(editedAjoneuvo);	
+			}
+			
+			// poista auto
+			@DeleteMapping("/ajoneuvot/{id}")
+			  public void deleteAjoneuvo(@PathVariable Long id) {
+			      arepository.deleteById(id);
+			  }
+				
+		}
+
--- src/test/java/s23/Kuljetuskalusto/KuljetuskalustoRestTests.java
+++ src/test/java/s23/Kuljetuskalusto/KuljetuskalustoRestTests.java
@@ -14,8 +14,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import s23.Kuljetuskalusto.domain.Ajoneuvo;
+
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,10 +49,9 @@ public class KuljetuskalustoRestTests {
 	
 	// Testaa että HTTP GET-pyyntö "/ajoneuvot" palauttaa vastauksen JSON-muodossa ja status että pyyntö on ok.
 	@Test
-	public void VastausJSONia() throws Exception {
+	public void GETinVastausJSONia() throws Exception {
 		mockMvc.perform(get("/ajoneuvot"))
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 	}
-	
-}
+}	
